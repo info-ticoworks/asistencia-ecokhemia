f5199c295e0d55e7dac69f35a5046c9352fddc7e
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-09 02:47:13 -0400

    Primer commit

--- frmHora.php
+++ frmHora.php
@@ -47,6 +47,8 @@ try {
         echo '<script>console.log("Paso 4")</script>';
         require_once './class/FechaPerfil.php';
         echo '<script>console.log("Paso 5")</script>';
+        require_once './class/noti_ingreso.php';
+        $noti_ingreso = new 
         $FechaPerfil = new FechaPerfil();
         $listR = $_POST["lista"];
         $cedula = ($_POST['ced']);
@@ -72,29 +74,7 @@ try {
                 $FechaPerfil->setDate($date);
                 $FechaPerfil->setUser($cedula);
                 echo '<p>' . $FechaPerfil->insertarHoraIngreso(). '</p>';
-
-                //Inicio de notificación por WhatsApp
-                $client = new http\Client;
-                $request = new http\Client\Request;
-                
-                $body = new http\Message\Body;
-                $body->append('{
-                  "message":"Hola de *nuevo*",
-                  "phone":"50683528129"
-                }');
-                
-                $request->setRequestUrl('http://51.222.14.197:3020/lead');
-                $request->setRequestMethod('POST');
-                $request->setBody($body);
-                
-                $request->setHeaders([
-                  'Content-Type' => 'application/json'
-                ]);
-                
-                $client->enqueue($request)->send();
-                $response = $client->getResponse();
-                //Final de notificación por WhatsApp
-
+                echo '<p>' . $request->send(). '</p>';
                 echo "<script>
             Swal.fire({
             icon: 'success',
---
+++ noti_ingreso.php
@@ -0,0 +1,22 @@
+<?php
+
+$request = new HttpRequest();
+$request->setUrl('http://51.222.14.197:3020/lead');
+$request->setMethod(HTTP_METH_POST);
+
+$request->setHeaders([
+  'Content-Type' => 'application/json'
+]);
+
+$request->setBody('{
+  "message":"Hola de *nuevo*",
+  "phone":"50683528129"
+}');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
