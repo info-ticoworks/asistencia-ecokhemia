290816001c6e6be9d40d31f0fdeaf7b17057c7ee
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-09 02:37:06 -0400

    Primer commit

--- frmHora.php
+++ frmHora.php
@@ -74,26 +74,25 @@ try {
                 echo '<p>' . $FechaPerfil->insertarHoraIngreso(). '</p>';
 
                 //Inicio de notificación por WhatsApp
-                $request = new HttpRequest();
-                $request->setUrl('http://51.222.14.197:3020/lead');
-                $request->setMethod(HTTP_METH_POST);
-
-                $request->setHeaders([
-                'Content-Type' => 'application/json'
-                ]);
-
-                $request->setBody('{
-                "message":"Hola de *nuevo*",
-                "phone":"50683528129"
+                $client = new http\Client;
+                $request = new http\Client\Request;
+                
+                $body = new http\Message\Body;
+                $body->append('{
+                  "message":"Hola de *nuevo*",
+                  "phone":"50683528129"
                 }');
-
-                try {
-                $response = $request->send();
-
-                echo $response->getBody();
-                } catch (HttpException $ex) {
-                echo $ex;
-                }
+                
+                $request->setRequestUrl('http://51.222.14.197:3020/lead');
+                $request->setRequestMethod('POST');
+                $request->setBody($body);
+                
+                $request->setHeaders([
+                  'Content-Type' => 'application/json'
+                ]);
+                
+                $client->enqueue($request)->send();
+                $response = $client->getResponse();
                 //Final de notificación por WhatsApp
 
                 echo "<script>
