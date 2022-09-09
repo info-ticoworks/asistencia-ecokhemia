3d5953793ab2f22c9ee255a09bbcd58a9d4ad5cd
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-09 02:04:17 -0400

    Primer commit

--- config.php
+++ config.php
@@ -1,26 +1,7 @@
 <?php
 try {
 echo '<script>console.log("Paso 3")</script>';
-$host = "107.180.13.125";
-$usuario = "asist-ecok";
-$clave = "CkowfYQ34JJdQ8Um4ILE";
-$bd="asistencia-ecokhemia";
-$conexion = mysqli_connect($host, $usuario, $clave, $bd);
-echo '<script>console.log("Paso 4")</script>';
-//$servername = "107.180.13.125";
-//$username = "asist-ecok";
-//$password = "CkowfYQ34JJdQ8Um4ILE";
-//$bd="asistencia-ecokhemia";
 
-// Create connection
-//$conexion = new mysqli($servername, $username, $password, $bd);
-
-// Check connection
-if ($conexion->connect_error) {
-  die("Connection failed: " . $conexion->connect_error);
-} else {
-       echo '<script>console.log("TicoWorks says: Conexión Establecida 07-08-2022")</script>';
-}
 // echo "Connected successfully";
 } catch (Exception $e) {
   log_exception($e);
