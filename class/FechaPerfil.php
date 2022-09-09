ce610f48c9673bab7c5359dc948fe4f5bad33a0e
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-09 02:23:22 -0400

    Primer commit

--- class/FechaPerfil.php
+++ class/FechaPerfil.php
@@ -103,28 +103,7 @@ function insertarFechaPerfil() {
             }
             $conexion->close();
             
-            //Inicio de notificación por WhatsApp
-            $request = new HttpRequest();
-            $request->setUrl('http://51.222.14.197:3020/lead');
-            $request->setMethod(HTTP_METH_POST);
 
-            $request->setHeaders([
-            'Content-Type' => 'application/json'
-            ]);
-
-            $request->setBody('{
-            "message":"Hola de *nuevo*",
-            "phone":"50683528129"
-            }');
-
-            try {
-            $response = $request->send();
-
-            echo $response->getBody();
-            } catch (HttpException $ex) {
-            echo $ex;
-            }
-            //Final de notificación por WhatsApp
             
         } else {
              echo "<script>
