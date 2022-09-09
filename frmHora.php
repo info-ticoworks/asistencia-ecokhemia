4cc6ba6c702d65d00afa1b73ba86a349574ff02f
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-09 01:52:22 -0400

    Primer commit

--- frmHora.php
+++ frmHora.php
@@ -35,7 +35,7 @@
                            <!-- <input type="checkbox" name="00">
                            <p>¿Mantener sesion iniciada?</p>--> 
         <input type="submit" value="Enviar" name="btEnviar" id="btEnviar">
-        <p>¿Ya has registrado la hora? <a class="" href="./asistencia-ecokhemia/">Volver atras</a></p>
+        <p>¿Ya has registrado la hora? <a class="" href="./">Volver atras</a></p>
         <input type="hidden" id="Ub" name="Ub" readonly>
     </form>
     <?php
