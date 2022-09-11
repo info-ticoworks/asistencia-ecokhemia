7b2a9a9cdbf1890f567e2cbb202553149dbd96bd
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-11 15:43:59 -0400

    Primer commit

--- frmHora.php
+++ frmHora.php
@@ -61,6 +61,47 @@ try {
 
 
 
+        $sql = "SELECT * FROM Perfiles";
+        if($result = mysqli_query($conexion, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                echo "<table>";
+                    echo "<tr>";
+                        echo "<th>Cedula</th>";
+                        echo "<th>Nombre</th>";
+                    echo "</tr>";
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                        echo "<td>" . $row['Cedula'] . "</td>";
+                        echo "<td>" . $row['Nombre'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                // Free result set
+                mysqli_free_result($result);
+            } else{
+                echo "No records matching your query were found.";
+            }
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conexion);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         $n = "SELECT COUNT(Nombre) as contar from Perfiles where Cedula = '$cedula'";
         $consultaN = mysqli_query($conexion, $n);
         $colaborador = mysqli_fetch_array($consultaN);
