c719aae3f86f533c96573a058983ed3f676b5d02
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-09-12 22:14:51 -0400

    Primer commit

--- frmHora.php
+++ frmHora.php
@@ -50,14 +50,18 @@
 
     function getAddress($latitude, $longitude)
     {
-            //google map api url
-            $url = "http://maps.google.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=AIzaSyD2uzWiIgPtw5ImJnrDkxA2kT1Pfcg0Ipk";
+        try {
+                //google map api url
+                $url = "http://maps.google.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=AIzaSyD2uzWiIgPtw5ImJnrDkxA2kT1Pfcg0Ipk";
 
-            // send http request
-            $geocode = file_get_contents($url);
-            $json = json_decode($geocode);
-            $address = $json->results[0]->formatted_address;
-            return $address;
+                // send http request
+                $geocode = file_get_contents($url);
+                $json = json_decode($geocode);
+                $address = $json->results[0]->formatted_address;
+                return $address;
+            } catch (Exception $e) {
+                log_exception($e);
+        }
     }
 
     try {
@@ -81,7 +85,7 @@
             //echo 'Latitud: ' . $latitude;
             //echo 'Longitud: ' . $longitude;
             $result = getAddress($latitude, $longitude);
-            echo 'Address: ' . $address;
+            echo 'Address: ' . $result;
 
 // produces output
 // Address: 58 Brooklyn Ave, Brooklyn, NY 11216, USA
